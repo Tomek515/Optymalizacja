f579234e0f8b78028e2a73a4a85b99567056c970
Tomek515 <149333002+Tomek515@users.noreply.github.com>
2025-05-03 15:21:39 +0200

    rownania sprzęzone

--- prhs.m
+++ prhs.m
@@ -28,7 +28,7 @@
 % num3 = term3 - term4;
 % dem3 = w^2 * (2 * R * x3 - x3^2);
 % dp(3) = -(num3 / dem3) * p(3);
-% dp = [dp(1); dp(2); dp(3)];
+% dp = -[dp(1); dp(2); dp(3)];
 
 function dp = prhs(t, x, u, p)
     % Parametry
@@ -42,38 +42,83 @@ function dp = prhs(t, x, u, p)
     c2 = 19; % stała
     c3 = 21; % stała
 
-    % Zabezpieczenia przed dzieleniem przez zero:
-    if x(1) <= 0
-        x(1) = eps;
-    end
-    if x(2) <= 0
-        x(2) = eps;
-    end
-    if (2*R*x(3) - x(3)^2) <= 0
-        x(3) = R/2; % bezpieczna wartość
-    end
 
-    % Liczymy pochodne po zmiennych stanu:
+    % if x(1) <= 0
+    %     x(1) = eps;
+    % end
+    % if x(2) <= 0
+    %     x(2) = eps;
+    % end
+    % if (2*R*x(3) - x(3)^2) <= 0
+    %     x(3) = R/2; 
+    % end
+
+
+
+
+   
+    x1 = x(1);
+    x2 = x(2);
+    x3 = x(3);
+    p1 = p(1);
+    p2 = p(2);
+    p3 = p(3);
     
-    % df1/dx1
-    df1dx1 = -(c1/(2*c*w*sqrt(x(1))));
+    % dp1
+    dp1 = -(4*p1/(35*sqrt(x1)) - (10*p2)/(sqrt(x1)*((69*x2)/20 + 10)));
 
-    % df2/dx1 i df2/dx2
-    df2dx1 = (c1/(2*w*((b-a)*x(2)/H + a)*sqrt(x(1))));
-    df2dx2 = -(c2*sqrt(x(2))*((b-a)/(H*w*((b-a)*x(2)/H + a)^2))) ...
-             + (-(c2/(2*w*((b-a)*x(2)/H + a)*sqrt(x(2)))));
+    % dp2
+    dp2 = -((p2*(2760*sqrt(x1)*sqrt(x2) - 1311*x2 + 3800)*10) / ...
+            (sqrt(x2)*(69*x2 + 200)^2) ...
+            - (19*sqrt(5)*p3) / (7*sqrt(x2)*sqrt(-x3*(5*x3 - 364))));
 
-    % df3/dx2 i df3/dx3
-    df3dx2 = (c2/(2*w*sqrt(2*R*x(3) - x(3)^2)*sqrt(x(2))));
-    df3dx3 = -(w*((c2*sqrt(x(2)) - c3*sqrt(x(3))))*(R - x(3))) ...
-             / (w^2 * (2*R*x(3) - x(3)^2)^(3/2)) ...
-             - (c3/(2*w*sqrt(2*R*x(3) - x(3)^2)*sqrt(x(3))));
-
-    % Teraz zapisujemy pochodne układu sprzężonego:
-    dp1 = df1dx1 * p(1) + df2dx1 * p(2);
-    dp2 = df2dx2 * p(2) + df3dx2 * p(3);
-    dp3 = df3dx3 * p(3);
-
-    % Ujemne pochodne zgodnie z zasadami adjointów
+    % dp3
+    dp3 = -((6916*p3*sqrt(x2) + 105*p3*x3^(3/2) - 190*p3*sqrt(x2)*x3) / ...
+            (35*((364*x3)/5 - x3^2)^(3/2)));
+    
     dp = -[dp1; dp2; dp3];
 end
+
+% function dp = prhs(t, x_val, u_val, p_val)
+%     % Definicja zmiennych symbolicznych
+%     syms x1 x2 x3 u real
+% 
+%     % Stałe
+%     a = 10; % cm
+%     b = 44.5; % cm
+%     c = 25; % cm
+%     w = 3.5; % cm
+%     H = 35; % cm
+%     R = 36.4; % cm
+%     c1 = 20;
+%     c2 = 19;
+%     c3 = 21;
+% 
+%     % Definicja funkcji f1, f2, f3 (prawe strony oryginalnych równań)
+%     f1 = (u - c1 * sqrt(x1)) / (c * w);
+%     f2 = (c1 * sqrt(x1) - c2 * sqrt(x2)) / (w * ((b - a) * x2 / H + a));
+%     f3 = (c2 * sqrt(x2) - c3 * sqrt(x3)) / (w * sqrt(2*R*x3 - x3^2));
+% 
+%     % Wektor funkcji
+%     f = [f1; f2; f3];
+% 
+%     % Zmienna stanu
+%     X = [x1; x2; x3];
+% 
+%     % Liczenie Jacobiego (macierz pochodnych cząstkowych df/dx)
+%     J = jacobian(f, X);
+% 
+%     % Podstawienie wartości rzeczywistych (x_val, u_val)
+%     J_num = double(subs(J, [x1, x2, x3, u], [x_val(1), x_val(2), x_val(3), u_val]));
+% 
+%     % Liczenie dp
+%     dp = J_num.' * p_val; % uwaga: transpozycja, bo dp = (Jacobian)' * p
+% 
+%     % Zwrócenie minus dp
+%     dp = -dp;
+% end
+
+% function dp = prhs(t, x_val, u_val, p_val, jac_func)
+%     J_num = jac_func(x_val, u_val); % szybkie liczenie macierzy Jacobiego
+%     dp = -J_num.' * p_val;          % sprzężone równanie: dp/dt = -Jᵀ * p
+% end
--- rhs.m
+++ rhs.m
@@ -17,31 +17,46 @@ function dx = rhs(t, x, u)
     %dx2 = (20 * sqrt(x1) - 19 * sqrt(x2)) / (3.45 * x2 + 10);
     %dx3 = (19 * sqrt(x2) - 21 * sqrt(x3)) / (3.45 * sqrt(72.8 * x3 - x3^2));
 
-    if x1 < 0
-        x1 = 0;
-    end
-    if x2 < 0
-        x2 = 0;
-    end
-    if x3 < 0
-        x3 = 0;
-    end
-    %max wysokosc
-    if x1 > H
-        x1 = H;
-    end
-    if x2 > H
-        x2 = H;
-    end
-    if x3 > R
-        x3 = R;
-    end
+    % if x1 < 0
+    %     x1 = 0;
+    % end
+    % if x2 < 0
+    %     x2 = 0;
+    % end
+    % if x3 < 0
+    %     x3 = 0;
+    % end
+    % %max wysokosc
+    % if x1 > H
+    %     x1 = H;
+    % end
+    % if x2 > H
+    %     x2 = H;
+    % end
+    % if x3 > R
+    %     x3 = R;
+    % end
 
     dx1 = (u - c1 * sqrt(x1)) / (c*w);
     dx2 = (c1 * sqrt(x1) - c2 * sqrt(x2)) / ((w*((b-a)*x2/H))+a);
     dx3 = (c2 * sqrt(x2) - c3 * sqrt(x3)) / (w * sqrt(2*R * x3 - x3^2));
     %min wysokosc
-    
+
     dx = [dx1; dx2; dx3];
 end
 
+% function [f, X, u] = rhs()
+%     syms x1 x2 x3 u real
+% 
+%     Stałe
+%     a = 10; b = 44.5; c = 25; w = 3.5; H = 35; R = 36.4;
+%     c1 = 20; c2 = 19; c3 = 21;
+% 
+%     Dynamika (uwaga: bez ifów ograniczających zakresy)
+%     dx1 = (u - c1 * sqrt(x1)) / (c * w);
+%     dx2 = (c1 * sqrt(x1) - c2 * sqrt(x2)) / (w * ((b - a)*x2/H + a));
+%     dx3 = (c2 * sqrt(x2) - c3 * sqrt(x3)) / (w * sqrt(2*R*x3 - x3^2));
+% 
+%     f = [dx1; dx2; dx3];
+%     X = [x1; x2; x3];
+% end
